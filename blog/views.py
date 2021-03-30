f4dc506c6e70088af7b9ffc75c18f658ddea35de
zhio <gpfer@qq.com>
2021-03-30 15:47:00 +0800

    基本框架搭建完成

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list
+    })
