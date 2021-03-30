5c3066e56113d07d06b74cbd865914604e6fc6e6
zhio <gpfer@qq.com>
2021-03-30 16:54:31 +0800

    后台修改

--- blog/models.py
+++ blog/models.py
@@ -3,36 +3,54 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField('名称', max_length=100)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField('名称', max_length=100)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=70)
+    title = models.CharField('标题', max_length=70)
 
-    body = models.TextField()
+    body = models.TextField('正文')
 
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
 
     # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
-    excerpt = models.CharField(max_length=200, blank=True)
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag,blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
